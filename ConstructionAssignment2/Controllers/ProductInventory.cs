a616103c0ec9c7a95b78e8eeab88a59dbdd07047
marzeroman <82726731+marzeroman@users.noreply.github.com>
2023-06-24 00:51:55 +0300

    Add files via upload

---
+++ ConstructionAssignment2/Controllers/ProductInventory.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ConstructionAssignment2.Models;
+
+namespace ConstructionAssignment2.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private static List<Product> _products = new List<Product>();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetProducts()
+        {
+            return Ok(_products);
+        }
+
+        [HttpPost]
+        public ActionResult<Product> CreateProduct(Product product)
+        {
+            product.Id = _products.Count + 1;
+            _products.Add(product);
+            return CreatedAtAction(nameof(GetProductById), new { id = product.Id }, product);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProductById(int id)
+        {
+            var product = _products.Find(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateProduct(int id, Product updatedProduct)
+        {
+            var product = _products.Find(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            product.Name = updatedProduct.Name;
+            product.Price = updatedProduct.Price;
+            product.Quantity = updatedProduct.Quantity;
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            var product = _products.Find(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            _products.Remove(product);
+            return NoContent();
+        }
+    }
+}
